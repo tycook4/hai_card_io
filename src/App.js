5168a5be145adcb22b811898304eb2bcfb13fb06
anaribeiros <anaribeiro3105@gmail.com>
2025-04-13 11:15:15 -0400

    index page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="launch-page">
+        <h1 className="title">Card.io</h1>
+        <button className="start-button">Start</button>
+        <div className="decorative-elements">
+          <div className="curve curve-1"></div>
+          <div className="curve curve-2"></div>
+          <div className="curve curve-3"></div>
+          <div className="curve curve-4"></div>
+          <div className="curve curve-5"></div>
+          <div className="curve curve-6"></div>
+        </div>
+      </div>
     </div>
   );
 }
